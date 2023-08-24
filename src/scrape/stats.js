ab788aa5ce4535a5e13312a62960f34eaac24fb8
JAMES CARVALLYO <jpcarvallyo@gmail.com>
2023-08-24 12:08:21 -0700

    make a processor file and create to fn to handle rowData and others like it.

--- src/scrape/stats.js
+++ src/scrape/stats.js
@@ -1,12 +1,8 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const {
-  seasonStatsStructure,
-  categoryType,
-  careerStatsStructure,
-} = require("../utils/constants.js");
+const { categoryType } = require("../utils/constants.js");
 const { tableQuery, rowQuery } = require("../utils/cheerioQueries.js");
-const { modeProcessor } = require("../utils/mode.js");
+const { rowsData } = require("../utils/processors.js");
 
 async function getStats(config) {
   const {
@@ -26,44 +22,12 @@ async function getStats(config) {
     })[0];
 
     const rows = $(tableStats).find(rowQuery(isSeason, isCareer));
-    const seasons = [];
-
-    rows.each((index, element) => {
-      if (isCareer || index >= 2) {
-        const obj = Object.assign(
-          {},
-          isSeason ? seasonStatsStructure.hitting : careerStatsStructure.hitting
-        );
-        const columns = $(element).find("td");
-        const rowData = [];
-
-        columns.each((colIndex, colElement) => {
-          rowData.push($(colElement).text().trim());
-        });
-
-        rowData.forEach((item, index, array) => {
-          // todo
-          modeProcessor(isSeason, isCareer, item, index, obj);
-        });
-
-        seasons.push(obj);
-      }
-    });
-
-    let statsObj = {};
-    if (isSeason) {
-      seasons.splice(-2);
-      // return seasons array but remove last two indexes (those are career numbers and header)
-
-      statsObj = seasons.reduce((acc, curr) => {
-        if (curr.year.length === 4) {
-          acc[curr.year] = curr;
-        }
-        return acc;
-      }, {});
-    } else {
-      statsObj.career = seasons.at(0);
-    }
+    const rowConfig = {
+      isCareer,
+      isSeason,
+      $,
+    };
+    const statsObj = rowsData(rows, rowConfig);
 
     return statsObj;
   } catch (error) {
