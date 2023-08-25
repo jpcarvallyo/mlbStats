d9cac3660a52b1bd1ba5af746805b4193061580a
JAMES CARVALLYO <jpcarvallyo@gmail.com>
2023-08-25 16:03:51 -0700

    Create miscellaneous dir, move files into it

--- src/utils/miscellaneous/saveToDB.js
+++ src/utils/miscellaneous/saveToDB.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { connectDB, getDB } = require("../../database");
+const { connectDB, getDB } = require("../../../database");
 // connect to db;
 
 const savePlayerStats = async () => {
